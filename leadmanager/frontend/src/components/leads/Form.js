5d85bb3d988a7b08f7f34b261155320fb73f6d2c
Brian Presser <bmpresser@gmail.com>
2020-05-21 18:29:59 -0700

    Adding React frontend configs and components

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Form extends Component {
+  render() {
+    return (
+      <h1>Add leads form</h1>
+    )
+  }
+}
+
+export default Form;
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Leads extends Component {
+  render() {
+    return (
+      <h1>Add leads list</h1>
+    )
+  }
+}
+
+export default Leads;
